c750eff46520b52dd741fd3ceadf2960fbe13988
Thibaut SIGUENZA <tsiguenz@e2r2p3.clusters.42paris.fr>
2021-12-23 15:51:13 +0100

    testing hooks

--- test.c
+++ test.c
@@ -6,11 +6,12 @@
 /*   By: tsiguenz <tsiguenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 15:59:57 by tsiguenz          #+#    #+#             */
-/*   Updated: 2021/12/23 15:05:35 by tsiguenz         ###   ########.fr       */
+/*   Updated: 2021/12/23 15:50:26 by tsiguenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../mlx/mlx.h"
+#include <stdio.h>
 
 typedef struct	s_data {
 	void	*img;
@@ -20,6 +21,11 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
 void	my_mlx_pixel_put(t_data *data, int x, int y, unsigned int color)
 {
 	char	*dst;
@@ -28,21 +34,24 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, unsigned int color)
 	*(unsigned int *)dst = color;
 }
 
+int	key_hook(int keycode, t_vars *vars)
+{
+	printf("keycode = %d\n", keycode);
+}
+
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
+	t_vars 	vars;
 	t_data	img;
-	int	i = 0;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 700, 700, "fdf");
-	img.img = mlx_new_image(mlx, 500, 500);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 700, 700, "fdf");
+	img.img = mlx_new_image(vars.mlx, 500, 500);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
-	while (i < img.line_length)
-		my_mlx_pixel_put(&img, i++, 0, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 100, 100);
-	mlx_loop(mlx);
+	my_mlx_pixel_put(&img, 0, 0, 0x00FF0000);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 100, 100);
+	mlx_key_hook(vars.win, key_hook, &vars);
+	mlx_loop(vars.mlx);
 	return (0);
 }
