fb1b82fc2e7b65318d1a4f047d1e92ae81d3990f
tsiguenz <tsiguenz@student.42.fr>
2022-01-18 00:25:24 +0100

    add draw_line.c

---
+++ draw_line.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_line.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsiguenz <tsiguenz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/17 23:21:51 by tsiguenz          #+#    #+#             */
+/*   Updated: 2022/01/18 00:25:01 by tsiguenz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fdf.h"
+
+void	draw_line(int x1, int y1, int x2, int y2)
+{
+	int	ex;
+	int	ey;
+	int	dx;
+	int	dy;
+	int	Dx;
+	int	Dy;
+	int	i;
+	int	Xincr;
+	int	Yincr;
+}
