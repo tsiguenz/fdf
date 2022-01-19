bd14875bea654d7c325cecc1249cebd92ed0ec67
Thibaut SIGUENZA <tsiguenz@e2r7p3.clusters.42paris.fr>
2022-01-19 17:48:45 +0100

    idk

--- srcs/render_map.c
+++ srcs/render_map.c
@@ -6,7 +6,7 @@
 /*   By: tsiguenz <tsiguenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 16:57:19 by tsiguenz          #+#    #+#             */
-/*   Updated: 2022/01/19 12:21:01 by tsiguenz         ###   ########.fr       */
+/*   Updated: 2022/01/19 17:47:56 by tsiguenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,37 +30,36 @@ int	mlx_close_esc(int keycode, t_vars *vars)
 	return (0);
 }
 
-int	mouse_hook(int button, int x, int y, t_vars *vars)
-{
-	(void) button;
-	mlx_pixel_put(vars->mlx, vars->win, x, y, 0x00FFFFFF);
-	return (0);
-}
-
 void	isometric(t_maps map, t_data *img)
 {
 	(void) map;
 	(void) img;
-	int	x, y, l;
-	float u, v;	
-	l = 10;
-	x = 0;
-	y = 0;
+	int	i, j, l;
+	int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+	int	org = 200;
+	l = 20;
+	i = 0;
+	j = 0;
 	(void) l;
-	while (y < map.ymax)
+	(void) x2;
+	(void) y2;
+	while (j < map.ymax)
 	{
-		while (x < map.xmax)
+		while (i < map.xmax)
 		{
-			u = ((x - map.tab[y][x]) * cos(0.5)) * l;
-			v = (y + (x + map.tab[y][x]) * sin(0.5)) * l;
-		//	u = x*cos(0) + y*cos(0+2.1) + map.tab[y][x]*cos(0-2.1);
-		//	v = x*sin(0) + y*sin(0+2.1) + map.tab[y][x]*sin(0-2.1);
-			printf("u = %f, v = %f\n", u, v);
-			pixel_put(img, u, v, 0x00FFFFFF);
-			x++;
+		//	x1 = ((i - map.tab[j][i]) / sqrt(2)) * l;
+		//	y1 = ((i + 2 * j + map.tab[j][i]) / sqrt(6)) * l;
+			x1 = (i - j) * l / 2;
+			y1 = (i + j) * l / 2;
+			printf("u = %d, v = %d\n", x1, y1);
+			pixel_put(img, org + x1, org + y1, 0x00FFFFFF);
+		//	draw_line(img, org + x1, org + y1, org + x2, org + y2);
+			x2 = x1;
+			y2 = y1;
+			i++;
 		}
-		y++;
-		x = 0;
+		j++;
+		i = 0;
 	}
 }
 
@@ -77,10 +76,6 @@ int	render_map(t_maps map)
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
 	mlx_key_hook(vars.win, mlx_close_esc, &vars);
-	mlx_mouse_hook(vars.win, mouse_hook, &vars);
-//	draw_line(&img, 10, 10, 150, 150);
-//	draw_line(&img, 100, 200, 30, 10);
-//	draw_line(&img, 10, 200, 200, 10);
 	isometric(map, &img);
 	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 100, 100);
 	mlx_loop(vars.mlx);
